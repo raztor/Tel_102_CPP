92b117a0f8ef3e8f78be0cad03e94a09ae1f0c2c
raztor <contacto@webizy.cl>
2022-09-02 11:32:42 -0400

    Finalizacion practico1A-2019

---
+++ Clase/C1/practico-1-2016/practico-1.cpp
@@ -0,0 +1,40 @@
+//
+// Created by raztor on 01-09-22.
+//
+
+#include <cstdio>
+#include <iostream>
+
+float calc(float a, float b, int op) {
+    switch (op) {
+        case 1:
+            return a + b;
+        case 2:
+            return a - b;
+        case 3:
+            return a * b;
+        case 4:
+            return a / b;
+        default:
+            return 0;
+    }
+}
+
+float printcalc(){
+    float a, b;
+    int op;
+    std::cout << "Ingrese primer numero: " << std::endl;
+    std::cin >> a;
+    std::cout << "Ingrese operacion: \nSuma: 1\nResta: 2\nMultiplicacion: 3\nDivision: 4" << std::endl;
+    std::cin >> op;
+    std::cout << "Ingrese segundo numero: " << std::endl;
+    std::cin >> b;
+    std::cout << "Resultado: " << calc(a, b, op) << std::endl;
+    return 0;
+
+}
+
+int main() {
+    std::cout << printcalc() << std::endl;
+    return 0;
+}
